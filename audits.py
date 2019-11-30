983752eab02fffc62645a0c043da936b191352bc
Jeff Stock <jeffstock@MacBook-Air-6.local>
2019-11-30 09:58:06 -0800

    refactored.

--- audits.py
+++ audits.py
@@ -4,50 +4,73 @@ from csv_reader import *
 class Auditor(object):
 	"""docstring for Auditor"""
 
-	ended_contracts = ListHandler.gtnp_filter(Getters.get_contracts('./static/ended_contracts.csv'))
-	active_contracts = ListHandler.gtnp_filter(Getters.get_contracts('./static/contracts.csv'))
-	auwa = ListHandler.fte_filter_user_list(Getters.get_active_users('./static/user_data.csv'))
-	whitelist = Getters.get_raw_whitelist('./static/whitelist.csv')
+	e = './static/ended_contracts.csv'
+	a = './static/contracts.csv'
+	u = './static/user_data.csv'
+	w = './static/whitelist.csv'
+	rmac = './static/rem_mac.csv'
+	flmac = './static/flr_mac.csv'
+	flpc = './static/flr_pc.csv'
+	mpc = './static/rem_pc.csv'
+	
+	ended_contracts = ListHandler.gtnp_filter(Getters.get_contracts(e))
+	active_contracts = ListHandler.gtnp_filter(Getters.get_contracts(a))
+	auwa = ListHandler.fte_filter_user_list(Getters.get_active_users(u))
+	whitelist = Getters.get_raw_whitelist(w)
+	complete_mac_list = Finders.find_location_and_agency(Finders.find_fl_with_mac_assets(ListHandler.fte_filter_mac_assets(Getters.get_users_with_mac_assets(rmac, flmac)), auwa), active_contracts)
+	complete_pc_list = Finders.find_location_and_agency(Finders.find_fl_with_pc_assets(Getters.get_users_with_pc_assets(flpc, mpc), auwa), active_contracts)
+
 	contracts_whitelist = ListHandler.filter_whitelist(whitelist, 'Contract')
 	countries_whitelist = ListHandler.filter_whitelist(whitelist, 'L3_country')
-	complete_mac_list = Finders.find_location_and_agency(Finders.find_fl_with_mac_assets(ListHandler.fte_filter_mac_assets(Getters.get_users_with_mac_assets('./static/rem_mac.csv', './static/flr_mac.csv')), auwa), active_contracts)
-	complete_pc_list = Finders.find_location_and_agency(Finders.find_fl_with_pc_assets(Getters.get_users_with_pc_assets('./static/flr_pc.csv', './static/rem_pc.csv'), auwa), active_contracts)
+	
 
 
 	def l3_locations():
 		audit_type = 3
 		filtered_contracts = ListHandler.fixed_price_filter(ListHandler.payroll_filter(Auditor.active_contracts))
-		output = Finders.find_hourly_contracts_l3_countries(filtered_contracts, Auditor.countries_whitelist, Auditor.contracts_whitelist)
-		FileHandler.create_action_list(output, audit_type)
+		contracts_list = Finders.find_hourly_contracts_l3_countries(filtered_contracts, Auditor.countries_whitelist, Auditor.contracts_whitelist)
+
+		FileHandler.create_action_list(contracts_list, audit_type)
 		print('Audit complete. Type: ICs in L3 Countries')
 
 	def active_users_without_contracts():
 		audit_type = 1
-		output = Finders.find_users_without_contracts(Auditor.active_contracts, Auditor.auwa)
-		FileHandler.create_action_list(output, audit_type)
+		users = Finders.find_users_without_contracts(Auditor.active_contracts, Auditor.auwa)
+
+		FileHandler.create_action_list(users, audit_type)
 		print('\nAudit complete. Type: Users with systems access, but no contract')
 
 	def end_dates():
 		audit_type = 2
-		FileHandler.create_action_list(Finders.find_end_dates(Auditor.active_contracts), audit_type)
+		contracts_list = Finders.find_end_dates(Auditor.active_contracts)
+
+		FileHandler.create_action_list(contracts_list, audit_type)
 		print('Audit complete. Type: End dates')
 
 	def multiple_contracts():
 		audit_type = 4
-		FileHandler.create_action_list(Finders.find_multiple_contracts(Auditor.active_contracts), audit_type)
+		contracts_list = Finders.find_multiple_contracts(Auditor.active_contracts)
+
+		FileHandler.create_action_list(contracts_list, audit_type)
 		print('Audit complete. Type: users with multiple contracts')
 
 	def fls_with_assets():
 		audit_type = 5
-		FileHandler.create_action_list(ListHandler.remove_duplicates_from_nested_list(ListHandler.list_combiner(Auditor.complete_mac_list, Auditor.complete_pc_list)), audit_type)
+		users = ListHandler.remove_duplicates_from_nested_list(ListHandler.list_combiner(Auditor.complete_mac_list, Auditor.complete_pc_list))
+
+		FileHandler.create_action_list(users, audit_type)
 		print('Audit complete. Type: freelancers with Upwork assets')
 
 	def currently_whitelisted():
 		audit_type = 6
-		FileHandler.create_action_list(WhitelistUpdater.current_whitelist(Auditor.active_contracts, Auditor.contracts_whitelist), audit_type)
+		current_list = WhitelistUpdater.current_whitelist(Auditor.active_contracts, Auditor.contracts_whitelist)
+
+		FileHandler.create_action_list(current_list, audit_type)
 		print('Audit complete. Type: Currently Whitelisted\n')
+
 		audit_type = 7
-		FileHandler.create_action_list(WhitelistUpdater.update(Auditor.ended_contracts, Auditor.contracts_whitelist, Auditor.whitelist),audit_type)
+		updated_list = WhitelistUpdater.update(Auditor.ended_contracts, Auditor.contracts_whitelist, Auditor.whitelist)
+		FileHandler.create_action_list(updated_list ,audit_type)
 
 	def __init__(self, arg):
 		super(Auditor, self).__init__()
